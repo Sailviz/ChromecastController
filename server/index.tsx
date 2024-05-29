fadc4bf37c15da6f5f3d71a451dc4cdee75277ba
AlexPegg <alex@alexpegg.uk>
2024-05-29 16:16:56 +0100

    controls CC!!!

--- server/index.tsx
+++ server/index.tsx
@@ -3,10 +3,7 @@ import express, { Express } from 'express';
 import * as http from 'http';
 import next, { NextApiHandler } from 'next';
 import * as socketio from 'socket.io';
-import { Client } from 'castv2';
-import { Browser } from '@astronautlabs/mdns';
-
-
+const ChromecastAPI = require('chromecast-api')
 
 if (process.env.PORT) {
     var port: number = parseInt(process.env.PORT);
@@ -40,40 +37,17 @@ nextApp.prepare().then(async () => {
     });
 
 
-
-    new Browser('_googlecast._tcp')
-        .on('serviceUp', (service: any) => ondeviceup(service))
-        .on('serviceDown', (service: any) => console.log("Device down: ", service))
-        .start();
-
-    function ondeviceup(host: any) {
-        console.log(host)
-        var client = new Client();
-        client.connect(host, function () {
-            // create various namespace handlers
-            var connection = client.createChannel('sender-0', 'receiver-0', 'urn:x-cast:com.google.cast.tp.connection', 'JSON');
-            var heartbeat = client.createChannel('sender-0', 'receiver-0', 'urn:x-cast:com.google.cast.tp.heartbeat', 'JSON');
-            var receiver = client.createChannel('sender-0', 'receiver-0', 'urn:x-cast:com.google.cast.receiver', 'JSON');
-
-            // establish virtual connection to the receiver
-            connection.send({ type: 'CONNECT' });
-
-            // start heartbeating
-            setInterval(function () {
-                heartbeat.send({ type: 'PING' });
-            }, 5000);
-
-            // launch app
-            receiver.send({ type: 'LAUNCH', appId: '0AA4CA7E', requestId: 1 });
-
-            // display receiver status updates
-            receiver.on('message', function (data: any, broadcast: any) {
-                if (data.type = 'RECEIVER_STATUS') {
-                    console.log(data.status, broadcast);
-                }
-            });
-        });
-
-    }
+    var client = new ChromecastAPI();
+    client.on('device', function (device: any) {
+        console.log(device.friendlyName)
+        device.play("", "customreceiver", function (err: any) {
+            if (!err) console.log('Playing in your chromecast')
+        })
+        setTimeout(() => {
+            device.sendMessage({ type: 'clubId', clubId: "fc4923c2-3f2a-45c5-8a7e-078ea5a9693d" }, function (err: any) {
+                console.log(err)
+            })
+        }, 10000)
+    })
 
 });
